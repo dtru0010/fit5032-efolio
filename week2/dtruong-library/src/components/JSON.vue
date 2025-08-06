073d4befc011afbb1bbb318ae53fad3a0bde67d5
Daniel Truong <dtru0010@student.monash.edu>
2025-08-06 19:27:02 +1000

    Activity 2 & 3 Completed

--- week2/dtruong-library/src/components/JSON.vue
+++ week2/dtruong-library/src/components/JSON.vue
@@ -96,11 +96,13 @@ const showMessage = ref(false)
 // Activity 2: Get authors born after 1850
 const modernAuthors = computed(() => {
   // TODO: CODE TO FILTER ARRAY OF AUTHORS HERE
+  authors.filter((author) => author.birthYear > 1850)
 })
 
 // Activity 3: Get all famous works
 const allFamousWorks = computed(() => {
   // TODO: CODE TO GET ALL FAMOUS WORKS HERE
+  authors.flatMap((author) => author.famousWorks.map((work) => work.title))
 })
 
 // Activity 4: Find author by name
