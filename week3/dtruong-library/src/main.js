b4e831e2612aebd9995bf97ac6284f02e90da117
Daniel Truong <dtru0010@student.monash.edu>
2025-08-20 16:56:43 +1000

    activity 4 complete

--- week3/dtruong-library/src/main.js
+++ week3/dtruong-library/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './style.css'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
