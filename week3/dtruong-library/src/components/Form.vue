6e287bef0979580fe7df9b6ce7f98cc26d558690
Daniel Truong <dtru0010@student.monash.edu>
2025-08-20 16:44:15 +1000

    activity 2 complete

--- week3/dtruong-library/src/components/Form.vue
+++ week3/dtruong-library/src/components/Form.vue
@@ -1,8 +1,29 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="form">
     <h1>User Information Form / Credentials</h1>
+
+    <form>
+      <label for="username">Username:</label><br />
+      <input type="text" id="username" name="username" /><br />
+
+      <label for="password">Password:</label><br />
+      <input type="password" id="password" name="password" /><br />
+
+      <label for="isAustralian">Australian Resident?</label><br />
+      <input type="checkbox" id="isAustralian" name="isAustralian" /><br />
+
+      <label for="reason">Reason for Joining:</label><br /><br />
+      <textarea id="reason" name="reason" rows="3"></textarea><br />
+
+      <label for="gender">Gender:</label><br />
+      <select id="gender" name="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+    </form>
   </div>
 </template>
 
