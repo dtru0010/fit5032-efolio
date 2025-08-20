65e03297cf7aeac365d9ceaefe8d32f51c7cd7db
Daniel Truong <dtru0010@student.monash.edu>
2025-08-20 17:30:28 +1000

    activity 6 complete

--- week3/dtruong-library/src/components/Form.vue
+++ week3/dtruong-library/src/components/Form.vue
@@ -1,4 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const formData = ref({
+  username: '',
+  password: '',
+  isAustralian: false,
+  reason: '',
+  gender: 'male',
+})
+
+const submittedCards = ref([])
+
+const submitForm = () => {
+  submittedCards.value.push({ ...formData.value })
+}
+
+const clearForm = () => {
+  submittedCards.value = []
+}
+</script>
 
 <template>
   <!-- <div class="form">
@@ -97,6 +117,46 @@
       </div>
     </div>
   </div>
+
+  <div class="row mt-5" v-if="submittedCards.length">
+    <div class="d-flex flex-wrap justify-content-start">
+      <div
+        v-for="(card, index) in submittedCards"
+        :key="index"
+        class="card m-2"
+        style="width: 18rem"
+      >
+        <div class="card-header">User Information</div>
+
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Username: {{ card.username }}</li>
+          <li class="list-group-item">Password: {{ card.password }}</li>
+          <li class="list-group-item">
+            Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
+          </li>
+          <li class="list-group-item">Gender: {{ card.gender }}</li>
+          <li class="list-group-item">Reason: {{ card.reason }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+
+.list-group-item {
+  padding: 10px;
+}
+</style>
