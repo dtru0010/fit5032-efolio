ad9a62a25ffcda1b605bd605b363bac1aa8d4b53
Daniel Truong <dtru0010@student.monash.edu>
2025-09-06 00:07:20 +1000

    week 7 setup

--- week7/dtruong-library/README.md
+++ week7/dtruong-library/README.md
@@ -1,4 +1,4 @@
-# dtruong-auth-app
+# dtruong-library
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ week7/dtruong-library/src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useAuth, logout } from '@/composables/useAuth'
+import router from '@/router'
+const { isAuthenticated } = useAuth()
+
+const logoutUser = () => {
+  logout()
+  router.push({ path: '/login' })
+}
+</script>
+
+<template>
+  <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" active-class="active" aria-current="page"
+            >Home</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link" active-class="active">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" v-if="!isAuthenticated" class="nav-link" active-class="active"
+            >Login</router-link
+          >
+          <a v-else href="#" class="nav-link" @click.prevent="logoutUser">Logout</a>
+        </li>
+      </ul>
+    </header>
+  </div>
+  <hr class="full-divider" />
+</template>
+
+<style>
+.full-divider {
+  width: 100%;
+  border: none;
+  border-top: 1px solid #000000;
+  margin: 0;
+}
+</style>
