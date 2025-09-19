9812bb8eaf3dada42f3d5331eebd1614b89b3bf4
Daniel Truong <dtru0010@student.monash.edu>
2025-09-19 17:33:23 +1000

    firebase logout

--- week7/dtruong-library/src/components/Header.vue
+++ week7/dtruong-library/src/components/Header.vue
@@ -1,11 +1,32 @@
 <script setup>
 import { useAuth, logout } from '@/composables/useAuth'
 import router from '@/router'
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth'
+import { onMounted, ref } from 'vue'
 const { isAuthenticated } = useAuth()
 
 const logoutUser = () => {
   logout()
   router.push({ path: '/login' })
+}
+
+const isLoggedIn = ref(false)
+const auth = getAuth()
+
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    console.log('Auth state changed:', user)
+    isLoggedIn.value = !!user
+  })
+})
+
+const handleSignOut = async () => {
+  try {
+    await signOut(auth)
+    router.push('/firebaseSignin')
+  } catch (error) {
+    console.error('Error signing out:', error)
+  }
 }
 </script>
 
@@ -27,16 +48,21 @@ const logoutUser = () => {
           >
           <a v-else href="#" class="nav-link" @click.prevent="logoutUser">Logout</a>
         </li>
-        <li class="nav-item">
-          <router-link to="/FireLogin" class="nav-link" active-class="active"
-            >Firebase Login</router-link
-          >
+        <li class="nav-item" v-if="isLoggedIn">
+          <a href="#" class="nav-link" @click.prevent="handleSignOut">Firebase Logout</a>
         </li>
-        <li class="nav-item">
-          <router-link to="/FireRegister" class="nav-link" active-class="active"
-            >Firebase Register</router-link
-          >
-        </li>
+        <template v-else>
+          <li class="nav-item">
+            <router-link to="/FireLogin" class="nav-link" active-class="active"
+              >Firebase Login</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/FireRegister" class="nav-link" active-class="active"
+              >Firebase Register</router-link
+            >
+          </li>
+        </template>
       </ul>
     </header>
   </div>
