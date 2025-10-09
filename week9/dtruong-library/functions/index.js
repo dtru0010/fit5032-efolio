89fe8d434ce21584860a76306d2424cfc5f5d646
Daniel Truong <dtru0010@student.monash.edu>
2025-10-09 21:21:38 +1100

    complete 9.4

--- week9/dtruong-library/functions/index.js
+++ week9/dtruong-library/functions/index.js
@@ -7,9 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const { setGlobalOptions } = require('firebase-functions')
+const { onRequest } = require('firebase-functions/https')
+const logger = require('firebase-functions/logger')
+
+const admin = require('firebase-admin')
+const cors = require('cors')({ origin: true })
+
+admin.initializeApp()
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -21,7 +26,7 @@ const logger = require("firebase-functions/logger");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({ maxInstances: 10 })
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -30,3 +35,17 @@ setGlobalOptions({ maxInstances: 10 });
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.countBooks = onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const booksCollection = admin.firestore().collection('books')
+      const snapshot = await booksCollection.get()
+      const count = snapshot.size
+      res.status(200).send({ count })
+    } catch (error) {
+      console.error('Error fetching book count:', error.message)
+      res.status(500).send('Failed to fetch book count')
+    }
+  })
+})
